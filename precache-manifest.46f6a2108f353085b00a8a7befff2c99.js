c0478d01afba70ef80f9083cfb7ae622969b4a32
Jismon Roy <jismonroy6@gmail.com>
2022-04-16 22:15:47 +0530

    Updates

---
+++ precache-manifest.46f6a2108f353085b00a8a7befff2c99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "27a4cb8e166f37de3e64",
+    "url": "/TODOAPP/static/css/main.ef91da48.chunk.css"
+  },
+  {
+    "revision": "27a4cb8e166f37de3e64",
+    "url": "/TODOAPP/static/js/main.27a4cb8e.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TODOAPP/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "8311894ec8b499d21889",
+    "url": "/TODOAPP/static/js/2.8311894e.chunk.js"
+  },
+  {
+    "revision": "93dbc7fc1353c1fa2acb",
+    "url": "/TODOAPP/static/js/runtime~main.93dbc7fc.js"
+  },
+  {
+    "revision": "0f55b6986bab74c4aef7c35af2aa116e",
+    "url": "/TODOAPP/index.html"
+  }
+];
